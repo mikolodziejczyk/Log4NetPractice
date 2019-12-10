5767fc480930323f8bdf11993d5f38ab3f24b5fb
mko <gogs@fake.local>
2019-12-10 09:42:29 +0100

    Add project files.

---
+++ Log4NetPractice/Invoice.cs
@@ -0,0 +1,31 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Log4NetPractice
+{
+    class Invoice
+    {
+
+        public void Issue()
+        {
+
+            for (int i = 0; i < 20; i++)
+            {
+
+
+                try
+                {
+                    if (i == 10) throw new InvalidOperationException("An error ocurred when issuing an item.");
+                }
+                catch (InvalidOperationException e)
+                {
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Log4NetPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Log4NetPractice
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+
+            // log start here
+
+
+            // log Login here
+
+            Invoice i = new Invoice();
+            i.Issue();
+
+            // log Logout here
+
+            // log exit here
+
+        }
+    }
+}
